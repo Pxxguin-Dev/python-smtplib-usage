5c6db706cb2170a15ec0f4726179ed282cbf9601
pxxguin <janghongseo@naver.com>
2026-07-21 15:44:58 +0900

    chore: new slack features are added

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from features.email_logic import EmailSender
+from features.slack_logic import SlackSender
 
 if __name__=='__main__':
     EmailSender()._sending_email()
+    SlackSender()._send_slack_message()
